980eea3f55aab01f9235d7ff6d482b522b4cbeb9
vyshnav s <vyshnavs0003@gmail.com>
2026-07-22 10:28:42 +0530

    feat:add protected user profile API

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 # Create your views here.
+
+class CurrentUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "username": request.user.username,
+            "message": "Authenticated successfully"
+        })
