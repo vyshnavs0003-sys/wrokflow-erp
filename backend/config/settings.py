85d819a216338e81b9573e0cca2e3eef0482d58e
vyshnav s <vyshnavs0003@gmail.com>
2026-07-22 10:09:35 +0530

    feat: configure JWT authentication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -129,3 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES":
+    (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
